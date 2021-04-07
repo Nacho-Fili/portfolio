7d10bd7b0c42ca1be85474e00f1f89e79d8a6bc6
Ignacio Filipovskis <n.filipovskis@gmail.com>
2021-04-07 12:10:44 -0300

    Cambio en la forma de servir el index.html

--- api/serveStatic.js
+++ api/serveStatic.js
@@ -1,5 +1,15 @@
+const { response } = require("express")
 const path = require("path")
 
 module.exports = async (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    const filePath = path.join(__dirname, '../client/build/index.html')
+    const stat = fileSystem.statSync(filePath)
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Content-Length': stat.size
+    })
+
+    const readStream = fileSystem.createReadStream(filePath)
+    readStream.pipe(response)
 }
