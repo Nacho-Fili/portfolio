72a90d97a696f85319162c4dc63d4434b7ebcffd
Ignacio Filipovskis <n.filipovskis@gmail.com>
2021-04-07 11:32:22 -0300

    Manejo de CORS agregado, correcion de la ruta de la API corregida

--- client/src/services/mailService.js
+++ client/src/services/mailService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 export default function sendEmail(message){
-    const ENDPOINT = "https://ignacio-filipovskis.vercel.app/"
+    const ENDPOINT = "https://ignacio-filipovskis.vercel.app"
     
     return axios.post(`${ENDPOINT}/send-mail`, {
         "data": message
