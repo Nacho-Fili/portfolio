40130d30204acb68c8e70ace682d34eb61970196
Ignacio Filipovskis <n.filipovskis@gmail.com>
2021-06-09 20:51:47 -0300

    Logs agregados

--- api/sendMail.js
+++ api/sendMail.js
@@ -14,7 +14,9 @@ module.exports = async ({ body }, res) => {
 
     try{
         await transporter.send(mailOptions)
-        return res.status(200)
+        res.status(200)
+        console.log("Message sent " + res)
+        return res
     } catch(err) {
         console.log("An error has ocurred posting the message")
         return res.status(400).json({message: "An error has ocurred posting the message"})
